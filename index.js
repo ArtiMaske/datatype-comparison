56adae323c36a24f40c25a4e67ebad80580fe8e1
ArtiMaske <maske.arti95@gmail.com>
2022-09-23 12:25:54 +0200

    refactor code

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 function isEqual(obj1, obj2) {
-  if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+  let stringified1 = JSON.stringify(obj1);
+  let stringified2 = JSON.stringify(obj2);
+  const isObject = (obj) => {
+    return Object.prototype.toString.call(obj) === "[object Object]";
+  };
+  if (stringified1 === stringified2) {
     return true;
-  } else {
-    const isObject = (obj) => {
-      return Object.prototype.toString.call(obj) === "[object Object]";
-    };
-    if (!isObject(obj1) || !isObject(obj2)) {
-      return false;
-    }
-    let len = null;
-
-    //check if they're of the same length
-    if (Object.keys(obj1).length != Object.keys(obj2).length) {
-      return false;
-    } else {
-      len = JSON.stringify(obj1).length; //use any length
-    }
-    let match = 0; //store number of matched properties
-    let stringified = JSON.stringify(obj1);
-
-    let i = 0;
-    //loop through the variable
-    while (i < stringified.length) {
-      //check if character exists in the other object
-      if (JSON.stringify(obj2).includes(stringified[i])) {
-        match++; //increment the variable
-      }
-      i++;
-    }
-    //check if object length equals the number of matched properties
-    if (match === len) {
-      return true;
-    }
+  } else if (!isObject(obj1) || !isObject(obj2)) {
     return false;
   }
-}
+  let len = null;
 
+  //check if objects of the same length
+  if (Object.keys(obj1).length != Object.keys(obj2).length) {
+    return false;
+  } else {
+    len = stringified1.length; //use any length
+  }
+  let match = 0; //store number of matched properties
+  let i = 0; //loop through the variable
+
+  while (i < len) {
+    //check if character exists in the other object
+    if (stringified2[i].includes(stringified1[i])) {
+      match++; //increment the variable
+    }
+    i++;
+  }
+  //check if object length equals the number of matched properties
+  if (match === len) {
+    return true;
+  }
+  return false;
+}
+console.log(isEqual(10, "10"));
 console.log(isEqual({ a: 1 }, { a: 1 })); //true
 console.log(isEqual({ a: 1 }, { a: 2 })); //false
 console.log(isEqual({ a: 1 }, { b: 2 })); //false
